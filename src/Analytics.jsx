0c0387d790ab94494fadd6868c4679e6ec3fe640
Sankalp Sharma <sankalpsharma250@gmail.com>
2025-01-03 03:40:56 +0530

    domnain added

--- src/Analytics.jsx
+++ src/Analytics.jsx
@@ -15,7 +15,7 @@ export default function Analytics() {
     formData.append('file', file)
 
     try {
-      const response = await fetch('https://16.171.249.240/predict/', {
+      const response = await fetch('https://audioforensics.tech/predict/', {
         method: 'POST',
         body: formData,
       })
